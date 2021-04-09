137b05ef9349a6c02270b9e6c6490b92f2cbb4ba
Ishanvyas <vyasishanatc194@gmail.com>
2021-04-09 13:18:54 +0530

    Create preset page

---
+++ assets/js/preset-step-desc.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  // Configure/customize these variables.
+  var showChar = 704; // How many characters are shown by default
+
+  // if (window.matchMedia("(max-width: 374px)").matches) {
+  //   showChar = 194;
+  // }
+  
+  var ellipsestext = "";
+  var moretext = "Read More";
+  var lesstext = "Read Less";
+
+  $(".more").each(function () {
+    var content = $(this).html();
+
+    if (content.length > showChar) {
+      var c = content.substr(0, showChar);
+      var h = content.substr(showChar, content.length - showChar);
+
+      var html =
+        c +
+        '<span class="moreellipses">' +
+        ellipsestext +
+        '&nbsp;</span><span class="morecontent"><span>' +
+        h +
+        '</span>&nbsp;&nbsp; <div class="btn-more-div"> <a href="" class="morelink">' +
+        moretext +
+        "</a></span></div>";
+
+      $(this).html(html);
+    }
+  });
+
+  $(".morelink").click(function () {
+    if ($(this).hasClass("less")) {
+      $(this).removeClass("less");
+      $(this).html(moretext);
+    } else {
+      $(this).addClass("less");
+      $(this).html(lesstext);
+    }
+    $(this).parent().prev().toggle();
+    $(this).prev().toggle();
+    return false;
+  });
+});
